2781250fdb8fcd2b7e0c4e05479066a4f46ef977
Djincode <yaqubprviz@gmail.com>
2024-02-29 00:50:14 +0400

    finally finished

---
+++ yummy/script.js
@@ -0,0 +1,43 @@
+//Task1
+
+console.log("My name is Murad");
+
+//Task2
+
+const name = "Perviz";
+const surname ="Yagubov";
+
+console.log(name, surname);
+
+//Task3
+
+// person console $add
+
+
+//Task 4
+
+let bookPrice = 750;
+let bookAmount = 14;
+const sumvalue = bookAmount * bookPrice;
+
+console.log(sumvalue);
+
+
+//Task 5
+
+let kmhSpeed = 75;
+let mssSpeed = kmhSpeed * 1000 / 3600
+
+console.log(mssSpeed);
+
+//Task 6
+
+let burnDate = +prompt("Dogum ilinizi daxil edin");
+
+let daysAlive = (2024- burnDate)*365;
+
+alert(daysAlive);
+
+
+//task7
+
